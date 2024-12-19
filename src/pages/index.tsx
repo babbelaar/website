25201990fbd5e83ed8a034bfae634646caaf6aed
Tristan Alexander Gerritsen <code@thewoosh.org>
2024-12-19 21:12:50 +0100

    fix: Zorg dat de knop op de hoofdpagina leidt naar de juiste plek

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/handleiding/intro">
+            to="/docs/handleiding/Inleiding">
             Beginnen maar!
           </Link>
         </div>
